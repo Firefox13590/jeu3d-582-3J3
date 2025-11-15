92b1e34f3e8ee1b7205459a06a073fc409477afe
Firefox13590 <pharlyb@gmail.com>
2025-11-15 13:43:01 -0500

    navigation menu fonctionnel

--- tp/Assets/Scripts/MenuNavigation.cs
+++ tp/Assets/Scripts/MenuNavigation.cs
@@ -2,22 +2,39 @@ using UnityEngine;
 
 public class MenuNavigation : MonoBehaviour
 {
-    public GameObject menuPrincipal;
-    public GameObject menuParametres;
-    public GameObject menuCredits;
+    public RectTransform[] listeMenus;
 
     RectTransform rtrCanvas;
+    int menuActuel = 1, menuCible = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rtrCanvas = GetComponent<RectTransform>();
-
         Debug.Log(rtrCanvas.rect);
+
+        for(int i = 0; i < listeMenus.Length; i++)
+        {
+            listeMenus[i].sizeDelta = new Vector2(rtrCanvas.rect.width, rtrCanvas.rect.height);
+            listeMenus[i].anchoredPosition = new Vector2(rtrCanvas.rect.width * (i - 1), 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GoToMenu(int targetMenuIndex)
+    {
+        menuCible = targetMenuIndex;
+        Vector2 movement = listeMenus[menuActuel].anchoredPosition - listeMenus[menuCible].anchoredPosition;
+        Debug.Log("movement: " + movement);
+
+        foreach (RectTransform menu in listeMenus)
+        {
+            menu.anchoredPosition += movement;
+        }
+        menuActuel = menuCible;
     }
 }
