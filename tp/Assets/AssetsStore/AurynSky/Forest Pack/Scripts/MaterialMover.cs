109e2ade2fb0cb85fc87f068f9fb19bdf3172341
Firefox13590 <pharlyb@gmail.com>
2025-11-28 16:00:28 -0500

    COOKING W LE WATER

--- tp/Assets/AssetsStore/AurynSky/Forest Pack/Scripts/MaterialMover.cs
+++ tp/Assets/AssetsStore/AurynSky/Forest Pack/Scripts/MaterialMover.cs
@@ -10,10 +10,15 @@ public class MaterialMover : MonoBehaviour
     void Start()
     {
         rend = GetComponent<Renderer>();
+        //Debug.Log(Time.time);
+        //Debug.Log(Time.time * scrollSpeed);
     }
     void Update()
     {
+        //Debug.Log(Time.time);
         float offset = Time.time * scrollSpeed;
-        rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+        //Debug.Log(offset);
+        //rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+        rend.material.mainTextureOffset = new Vector2(0, offset);
     }
 }
